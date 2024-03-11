252e7e40bc7ac43459b79e84bfdf7337f84b2aae
Mehedad Rabbani <rabbanihosni10@gmail.com>
2024-03-12 03:40:58 +0600

    .as.

---
+++ Day1_ES6_intro/template.js
@@ -0,0 +1,13 @@
+const first='Jaan';
+const last='pakhi';
+const greet='potash potash'
+const name=first+" "+last+" "+greet;
+console.log(name); 
+const a =10;
+const b =20;
+const result='The sum of'+a+ 'and '+b+'is'+(a+b);
+//back tick4
+const math=`The sum of ${a} and ${b} is ${a+b}`;
+console.log(math)
+
+const email='Hi Jogn Sina'
