ff04078ef7a81708e06c58e46db2dc058cb7c4ee
Mehedad Rabbani <rabbanihosni10@gmail.com>
2024-03-12 17:05:16 +0600

    arrow function 1

--- Day1_ES6_intro/template.js
+++ Day1_ES6_intro/template.js
@@ -1,4 +1,5 @@
-const first='Jaan';
+// for template string or dynamic string we can use `` and ${}
+ const first='Jaan';
 const last='pakhi';
 const greet='potash potash'
 const name=first+" "+last+" "+greet;
@@ -6,8 +7,17 @@ console.log(name);
 const a =10;
 const b =20;
 const result='The sum of'+a+ 'and '+b+'is'+(a+b);
+const loki={name: 'Tobarok', degignation: 'tomarok'}
 //back tick4
-const math=`The sum of ${a} and ${b} is ${a+b}`;
-console.log(math)
+const math=`The sum of ${a} and ${b} is ${a+b} and ${loki.name}`;
+console.log(math);
 
-const email='Hi Jogn Sina'
+const email='Hi John Sina\n'+
+'john sina re sina sina lage\n'+
+'tumi maramari korba naki';
+console.log(email);
+const challenge=`John cena
+cena naki ochena
+mair dimu procur
+`
+console.log(challenge);
